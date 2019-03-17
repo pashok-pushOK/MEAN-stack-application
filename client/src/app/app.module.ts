8bb0bc4fa7effdcb26d43a906c9c1f30d02fc955
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-03-17 22:04:52 +0200

    front validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -26,7 +28,9 @@ import { FooterComponent } from './components/footer/footer.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {group} from "@angular/animations";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
+    submitted = false;
 
-  ngOnInit() {
-  }
+    validateUserName(control) {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        if(regExp.test(control.value)) {
+            return null;
+        } else {
+            return {'validateUserName': true};
+        }
+    }
+
+    validateUserEmail(control) {
+        const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        if(regExp.test(control.value)) {
+            return null;
+        } else {
+            return {'validateUserEmail': true};
+        }
+    };
+
+    validateUserPassword(control) {
+        const regExp = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})");
+        if(regExp.test(control.value)) {
+            return null;
+        } else {
+            return {'validateUserPassword': true};
+        }
+    };
+
+    validateUserCity(control) {
+        const regExp = new RegExp(/^[a-zA-Z\u0080-\u024F\s\/\-\)\(\`\.\"\']+$/);
+        if(regExp.test(control.value)) {
+            return null;
+        } else {
+            return {'validateUserCity': true};
+        }
+    };
+
+    createForm() {
+        this.form = this.formBuilder.group({
+            userName: ['',
+                Validators.compose([
+                    Validators.required,
+                    Validators.minLength(3),
+                    Validators.maxLength(20),
+                    this.validateUserName
+                ])
+            ],
+            userPassword: ['', Validators.compose([
+                Validators.required,
+                this.validateUserPassword
+            ])],
+            userRepeatPassword: ['', Validators.required],
+            userEmail: ['', Validators.compose([
+                Validators.required,
+                this.validateUserEmail
+            ])],
+            userCity: ['', Validators.compose([
+                Validators.required,
+                this.validateUserCity
+            ])],
+            userAdress: ''
+        }, {validator: this.matchingPasswords('userPassword', 'userRepeatPassword')});
+    }
+
+    matchingPasswords(pass, confirm) {
+        return (group: FormGroup) => {
+            if(group.controls[pass].value === group.controls[confirm].value) {
+                return null;
+            } else {
+                return {'matchingPasswords': true};
+            }
+        }
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        console.log(`The form has been submitted!`);
+    }
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.createForm();
+    }
 
 }
