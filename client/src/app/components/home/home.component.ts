6270b095fc6f92e295f383af672ad54b09f4854c
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-03-21 20:59:23 +0200

    ...

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    componentTexts = [
+        {
+            componentTitle: 'MEAN Stack Application',
+            componentDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, parturient montes, nascetur ridiculus mus.'
+        },
+        {
+            componentTitle: 'Slide 2. MEAN Stack Application Again.',
+            componentDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
