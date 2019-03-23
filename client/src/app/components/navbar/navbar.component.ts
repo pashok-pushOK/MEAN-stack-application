e21f98795a4c80dbc494dd0330bf64cfa624efd4
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-03-23 13:18:22 +0200

    remove loop links

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, Event, NavigationStart, NavigationEnd, NavigationError} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    public activeRoute;
 
-  ngOnInit() {
-  }
+    constructor(private router: Router, public route: ActivatedRoute) {
+        this.router.events.subscribe((event: Event) => {
+            if (event instanceof NavigationStart) {
+                // Show loading indicator
+                this.activeRoute = event.url;
+            }
+        //
+        //     if (event instanceof NavigationEnd) {
+        //         // Hide loading indicator
+        //     }
+        //
+        //     if (event instanceof NavigationError) {
+        //         // Hide loading indicator
+        //
+        //         // Present error to user
+        //         // console.log(event.error);
+        //     }
+        });
+    }
 
+    ngOnInit() {
+    }
 }
