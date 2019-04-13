a31a85c0d2d5dd7224b5deea36b7c4701ca47961
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-04-13 13:59:33 +0300

    fixes

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -7,8 +7,6 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    componentTitle: string = 'MEAN Stack Application';
-
     constructor() {
     }
 
