dc9bc5c5a8b00103e48d3731bf2dfb6ddfc87662
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-04-16 21:13:03 +0300

    changes

--- client/src/app/components/blog/blog.component.ts
+++ client/src/app/components/blog/blog.component.ts
@@ -1,5 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {BlogDataService} from "../../service/blog-data.service";
+import {PostsService} from "../../service/posts.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+
+class ImageSnippet {
+    constructor(
+        public src: string,
+        public file: File
+    ) {
+    }
+}
 
 @Component({
     selector: 'app-blog',
@@ -8,14 +18,77 @@ import {BlogDataService} from "../../service/blog-data.service";
 })
 export class BlogComponent implements OnInit {
 
+    // user data variables
+    userName;
+    userEmail;
+    userCity;
+    userAdress;
+    userId;
+
+    image: HTMLImageElement;
     blogCardsArr = [];
 
-    constructor(
-        private blogDataService: BlogDataService
-    ) {
+    // variables for creating a new blog post
+    selectedPostFile: ImageSnippet;
+    blogPostForm: FormGroup;
 
+    // autocomplete filed options
+    options: string[] = ['Beauty', 'Love', 'Sport', 'Nature', 'Adventure'];
+
+    constructor(
+        private blogDataService: BlogDataService,
+        private formBuilder: FormBuilder,
+        private postService: PostsService
+    ) {
+        // Create blog post form group on page loads
+        this.blogPostForm = this.formBuilder.group({
+            blogInputCategory: new FormControl(''),
+            blogInputTitle: new FormControl(''),
+            blogInputText: new FormControl(''),
+            blogInputImage: new FormControl('')
+        });
     }
 
+    uploadPostImage(postImageInput: any): void {
+        if (postImageInput.files || postImageInput.files[0]) {
+            let postImageUploadView = document.getElementById('postImageUploadView');
+            const reader = new FileReader();
+
+            reader.onload = (event: any) => {
+                this.selectedPostFile = new ImageSnippet(event.target.result, postImageInput.files[0]);
+
+                this.image = document.createElement('img');
+                this.image.src = this.selectedPostFile.src;
+                this.image.width = 200;
+                postImageUploadView.innerHTML = '';
+                postImageUploadView.appendChild(this.image);
+            };
+
+            if (postImageInput.files[0]) {
+                reader.readAsDataURL(postImageInput.files[0]);
+            }
+        }
+    }
+
+    // method for submitting user created blog post
+    public submitBlogPostForm() {
+        const postObject = {
+            blogInputCategory: this.blogPostForm.get('blogInputCategory').value,
+            blogInputTitle: this.blogPostForm.get('blogInputTitle').value,
+            blogInputText: this.blogPostForm.get('blogInputText').value,
+            blogInputImage: this.selectedPostFile.file,
+            blogDatePublication: new Date().toLocaleDateString(),
+            blogAuthorId: this.userId,
+            blogAuthorName: this.userName
+        };
+
+        this.postService.createPost(postObject)
+            .subscribe(res => {
+                console.log(res);
+            })
+    }
+
+    // get posts with service
     getBlogData() {
         this.blogCardsArr = this.blogDataService.getBlogData();
     }
--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../service/login.service";
 import {ImageService} from "../../service/image.service";
-import {PostsService} from "../../service/posts.service";
 import {
     FormBuilder,
     FormGroup,
@@ -23,8 +22,6 @@ class ImageSnippet {
 })
 export class ProfileComponent implements OnInit {
 
-    image: HTMLImageElement;
-
     // user data variables
     userName;
     userEmail;
@@ -40,29 +37,13 @@ export class ProfileComponent implements OnInit {
     selectedFile: ImageSnippet;
     form: FormGroup;
 
-    // variables for creating a new blog post
-    selectedPostFile: ImageSnippet;
-    blogPostForm: FormGroup;
-
-    // autocomplete filed options
-    options: string[] = ['Beauty', 'Love', 'Sport', 'Nature', 'Adventure'];
-
     constructor(
         private loginService: LoginService,
         private imageService: ImageService,
-        private formBuilder: FormBuilder,
-        private postService: PostsService
+        private formBuilder: FormBuilder
     ) {
         this.form = this.formBuilder.group({
             avatar: new FormControl('')
-        });
-
-        // Create blog post form group on page loads
-        this.blogPostForm = this.formBuilder.group({
-            blogInputCategory: new FormControl(''),
-            blogInputTitle: new FormControl(''),
-            blogInputText: new FormControl(''),
-            blogInputImage: new FormControl('')
         });
     }
 
@@ -115,45 +96,6 @@ export class ProfileComponent implements OnInit {
 
     logOut() {
         this.loginService.logOut();
-    }
-
-    uploadPostImage(postImageInput: any): void {
-        if(postImageInput.files || postImageInput.files[0]) {
-            let postImageUploadView = document.getElementById('postImageUploadView');
-            const reader = new FileReader();
-
-            reader.onload = (event: any) => {
-                this.selectedPostFile = new ImageSnippet(event.target.result, postImageInput.files[0]);
-
-                this.image = document.createElement('img');
-                this.image.src = this.selectedPostFile.src;
-                this.image.width = 200;
-                postImageUploadView.innerHTML = '';
-                postImageUploadView.appendChild(this.image);
-            };
-
-            if(postImageInput.files[0]) {
-                reader.readAsDataURL(postImageInput.files[0]);
-            }
-        }
-    }
-
-    // method for submitting user created blog post
-    public submitBlogPostForm() {
-        const postObject = {
-            blogInputCategory: this.blogPostForm.get('blogInputCategory').value,
-            blogInputTitle: this.blogPostForm.get('blogInputTitle').value,
-            blogInputText: this.blogPostForm.get('blogInputText').value,
-            blogInputImage: this.selectedPostFile.file,
-            blogDatePublication: new Date().toLocaleDateString(),
-            blogAuthorId: this.userId,
-            blogAuthorName: this.userName
-        };
-
-        this.postService.createPost(postObject)
-            .subscribe(res => {
-                console.log(res);
-            })
     }
 
     ngOnInit() {
