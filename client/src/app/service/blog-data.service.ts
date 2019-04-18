0e6957abd103d3e175ba5f8852f13b1d91eb8151
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-04-18 22:12:27 +0300

    article doesn't work yet

--- client/src/app/service/blog-data.service.ts
+++ client/src/app/service/blog-data.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {BlogCard} from "../blog-card";
 import {HttpClient} from "@angular/common/http";
+import {catchError, map, tap} from 'rxjs/operators';
 
 export interface posts {
     data: object;
@@ -12,35 +13,29 @@ export interface posts {
 })
 export class BlogDataService {
 
+    blogData: any = this.getPostsData()
+        .subscribe(res => {
+        this.blogData = res.data;
+    });
+    formData = new FormData();
+
     constructor(
         private http: HttpClient
     ) {
-    }
-
-    blogData: any;
-    formData = new FormData();
-
-    public getBlogData() {
-        this.getPostsData()
-            .subscribe(res => {
-                this.blogData = res.data;
-            });
-
-        return this.blogData;
     }
 
     public getPost(id: string): Observable<BlogCard> {
         return of(this.blogData.find(post => post._id === id));
     }
 
+    public getPostsData() {
+        return this.http.get<posts>(`http://localhost/blog/posts`)
+    }
+
     public createPost(post): Observable<posts> {
-        for(let key in post) {
+        for (let key in post) {
             this.formData.append(key, post[key]);
         }
         return this.http.post<posts>('http://localhost/blog/createNewArticle', this.formData);
-    }
-
-    public getPostsData(): Observable<posts> {
-        return this.http.get<posts>(`http://localhost/blog/posts`);
     }
 }
